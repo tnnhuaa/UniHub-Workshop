0582be01e9c16a006f6b7b94a9917bb8f964adf9
Gia Thành <71538542+vntthanh@users.noreply.github.com>
2026-05-14 21:06:01 +0700

    feat: update mobile configuration and dependencies for improved linting and type support

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -6,5 +6,12 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    rules: {
+      'import/namespace': 'off',
+      'import/no-unresolved': 'off',
+      'import/no-duplicates': 'off',
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+    },
   },
 ]);
